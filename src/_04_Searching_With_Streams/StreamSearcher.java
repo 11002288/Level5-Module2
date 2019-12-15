918e9c3a05fdfdfebfce1fe8eb0ecfe72af8ba87
League Student <league@iMac-26.attlocal.net>
2019-12-15 10:42:51 -0800

    done
    done

--- src/_04_Searching_With_Streams/StreamSearcher.java
+++ src/_04_Searching_With_Streams/StreamSearcher.java
@@ -10,12 +10,12 @@ public class StreamSearcher {
 	// use Streams to help us out.
 	public long containsCount(String[] strArr, String strToCount) {
 		//2. Convert the String array to a stream
-		Stream.of(strArr).filter());
+		return Stream.of(strArr).filter(e -> e.equals(strToCount)).count();
+			
 		//3. Use the filter method with a lambda to extract all the String
 		//   elements that match the String parameter.
 		
 		//4. Return the count of the filtered stream.
 		
-		return 0;
-	}
+			}
 }
